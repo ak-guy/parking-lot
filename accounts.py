77108f70c29e26f66bf01ccc5dad7a2eb9061d6c
Arpit <arpitkumar3203@gmail.com>
2024-01-18 23:35:09 +0530

    admin methods

--- accounts.py
+++ accounts.py
@@ -1,5 +1,6 @@
 # from abc import abstractmethod, ABC
 from . import Person
+
 class Account:
     def __init__(self, user_name, password, person: Person, account_status):
         self.__user_name: str = user_name
@@ -11,4 +12,26 @@ class Admin(Account):
     def __init__(self, user_name: str, password: str, person: Person, account_status):
         super().__init__(user_name, password, person, account_status)
     
-    
+    def add_parking_floor(self):
+        pass
+
+    def add_parking_spot(self):
+        pass
+
+    def add_display_board(self):
+        pass
+
+    def add_entrance_panel(self):
+        '''can make payment here too, also free parking spot will be shown in this'''
+        pass
+
+    def add_exit_panel(self):
+        '''can make payment here'''
+        pass
+
+class Attendent(Account):
+    def __init__(self, user_name: str, password: str, person: Person, account_status):
+        super().__init__(user_name, password, person, account_status)
+
+    def process_cash(self):
+        pass
